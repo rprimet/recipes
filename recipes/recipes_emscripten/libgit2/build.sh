78be56b42e8d01919bcdddae329aaf02abeb6387
Romain Primet <romain.primet@inria.fr>
2022-11-24 15:10:21 +0100

    yet more drafting

--- recipes/recipes_emscripten/libgit2/build.sh
+++ recipes/recipes_emscripten/libgit2/build.sh
@@ -4,5 +4,6 @@ cd build
 emcmake cmake .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib
-emcmake cmake --build .
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DUSE_HTTPS=OFF
+emmake make install
